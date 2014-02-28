9f50cc64160a190834d3b053969b4e164c0c322b
tsuyoshi-maeda <t.maeda@sonicmoov.com>
2014-02-28 13:37:58 +0900

    first commit

---
+++ git_flow.sh
@@ -0,0 +1,65 @@
+#/bin/sh
+
+# 引数 ------------------------------------------------------------
+
+# $1 : repository url
+# $2 : target_tag_name
+# $3 : next_set_tag_name
+# $4 : prefix (解凍したときのディレクトリ名)
+# $5 : zip_name (zipファイル名)
+
+# ----------------------------------------------------------------
+
+
+git_flow() {
+  local repository_url=$1
+	local target_tag_name=$2
+	local next_set_tag_name=$3
+	local prefix=$4
+	local zip_name=$5
+
+  local base_dir=$(cd `dirname $0`; pwd)
+	local clone_dir="$base_dir/clone_dir"
+
+	# git clone
+  echo "clone開始---------------"
+	git clone $repository_url $clone_dir
+  echo "clone終了---------------"
+
+  ## clonedirに移動
+	cd $clone_dir
+
+  # 第二引数のtag nameからリビジョンID取得
+  echo "リビジョンID取得開始---------------"
+	revision_id=`git show $target_tag_name | grep commit | cut -c8- | head -n 1`
+
+	echo $revision_id
+  echo "リビジョンID取得終了---------------"
+
+	echo "差分アーカイブ開始---------"
+
+  $base_dir/git_diff_archive.sh $revision_id $prefix $zip_name
+
+	# 次のタグセット
+	echo "新しくタグをセット: $next_set_tag_name -------------------"
+	git tag -a $next_set_tag_name -m "set tag $next_set_tag_name"
+	git push origin $next_set_tag_name
+
+	# 実行ファイルのディレクトリにzipを移動
+	mv $zip_name $base_dir
+
+	echo "差分アーカイブ終了---------"
+
+	## 元のディレクトリに戻って諸々削除
+	echo "cloenディレクトリ削除------------------"
+	cd $base_dir
+	rm -rf $clone_dir
+}
+
+if [ $# -eq 5 ]; 
+then
+  git_flow $1 $2 $3 $4 $5
+else
+	echo "引数エラー 1: repository url, 2: target_tag_name, 3: next_set_tag_name, 4: prefix 5: zip_name"
+	echo "使い方 : ./git_flow.sh /Users/maedatsuyoshi/Documents/git/repository/share.git v1 v2 root zip.zip"
+fi
